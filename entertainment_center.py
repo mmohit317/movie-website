b9845a2dcef74959724ccce417d027621c0293e0
Mohit Mehta <32294871+mmohit317@users.noreply.github.com>
2018-02-22 12:07:13 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,17 +1,17 @@
 import fresh_tomatoes
-#to import fresh_tomatoes.py which contains html code
+# to import fresh_tomatoes.py which contains html code
 import media
-#to import media.py which consists of class Movie
-#which is used below
-#here six movie objects are created initialised with title,
-#storyline,poster,imagelink,trailer link
+# to import media.py which consists of class Movie
+# which is used below
+# here six movie objects are created initialised with title,
+# storyline,poster,imagelink,trailer link
 firangi = media.Movie("Firangi",
                       "Firangi is a 2017 Indian Hindi period action-comedy "
                       "drama filmof about 200 years ago.",
                       "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/"
                       "Firangi_-_Poster_1.jpg/220px-Firangi_-_Poster_1.jpg",
                       "https://www.youtube.com/watch?v=C3GiqcWF5QE")
-#media.Movie is used to assign values to the class Movie which is in media.py
+# media.Movie is used to assign values to the class Movie which is in media.py
 omg = media.Movie("Oh My God",
                   "The Story of a Man who is affected by Act Of God",
                   "https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/"
@@ -49,7 +49,7 @@ newton = media.Movie("Newton",
                      "https://www.youtube.com/watch?v=yU6zMPFd4UU")
 movies = [firangi, omg, dangal, tiger_zinda_hai, zindagi_na_milegi_dobaara,
           newton]
-#stores objects in alist
+# stores objects in alist
 fresh_tomatoes.open_movies_page(movies)
-#fresh_tomatoes.open_movies_page(movies) opens the movie
-#website in default browser
+# fresh_tomatoes.open_movies_page(movies) opens the movie
+# website in default browser
